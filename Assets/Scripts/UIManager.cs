0ebd4703ae9d74e209742d56474bc3bbc8af86a4
yxie7333 <yxie7333@usc.edu>
2023-09-21 16:39:28 -0700

    Update UIManager Script

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,41 +1,6 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-
-//public class UIManager : MonoBehaviour
-//{
-//    public Player player;
-//    public GameObject gameOverPanel; // Drag your Game Over panel here in the inspector.
-//    public GameObject playerWonPanel; // Drag your Player Won panel here in the inspector.
-//    public Text totalPelletsText;
-//    public Text currentPelletsText;
-
-
-//    private void Update()
-//    {
-//        if (player != null)
-//        {
-//            totalPelletsText.text = "Total Pellets: " + player.totalPelletsCollected.ToString();
-//            currentPelletsText.text = "Current Pellets: " + player.currentPelletsCanUse.ToString();
-//        }
-//        switch (GameManager.instance.currentState)
-//        {
-//            case GameManager.GameState.Playing:
-//                if (playerWonPanel != null) playerWonPanel.SetActive(false);
-//                if (gameOverPanel != null) gameOverPanel.SetActive(false);
-//                break;
-//            case GameManager.GameState.GameOver:
-//                if (gameOverPanel != null) gameOverPanel.SetActive(true);
-//                break;
-//            case GameManager.GameState.PlayerWon:
-//                if (playerWonPanel != null) playerWonPanel.SetActive(true);
-//                break;
-//        }
-//    }
-//}
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -45,9 +10,10 @@ public class UIManager : MonoBehaviour
     public Player player;
     public GameObject winPanel;
     public GameObject lostPanel;
+
+
     private void Update()
     {
-        
         currentPellet.text = "Current Pellet: " + player.currentPelletsCanUse.ToString();
         totalPellet.text = "Total Pellet: " + player.totalPelletsCollected.ToString();
 
@@ -69,5 +35,6 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    
+
+
 }
